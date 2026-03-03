e258f311186caf1059fbb2a20ee1855c20690805
Gina <roes1635000@gmail.com>
2026-03-03 16:28:32 +0800

    修改綠界和餐點購物車 // POST: api/TMealCarts/Checkout/{userId}為訂購時間  創建時有存訂單狀態

--- gym-api/Controllers/meal/ECPayPaymentController.cs
+++ gym-api/Controllers/meal/ECPayPaymentController.cs
@@ -34,7 +34,10 @@ namespace gym_api.Controllers.meal
                 if (order == null)
                     return BadRequest("找不到訂單");
 
-                string merchantId = "3002607";
+                order.FOrderStatus = "已付款，待取餐";
+                _context.SaveChanges();
+
+            string merchantId = "3002607";
                 string hashKey = "pwFHCqoQZGmho4w6";
                 string hashIV = "EkRm7iFT261dpevs";
 
@@ -148,8 +151,7 @@ namespace gym_api.Controllers.meal
 
                 if (order != null)
                 {
-                    order.FOrderStatus = "已付款，待取餐";
-                    order.FOrderAt = DateTime.Now;
+                    order.FOrderStatus = "信用卡付款成功，待取餐";
                     _context.SaveChanges();
                 }
             }
--- gym-api/Controllers/meal/TMealCartsController.cs
+++ gym-api/Controllers/meal/TMealCartsController.cs
@@ -170,6 +170,7 @@ namespace gym_api.Controllers.meal
             order.FOrderEmail = dto.Email;
             order.FVenueId = dto.VenueId;
             order.FPayMethod = dto.PayMethod;
+            order.FOrderAt = DateTime.Now;
             order.FTotalAmount = order.TMealOrderItems.Sum(i => i.FSubtotal);
             //  在這裡產生 QRCode
             foreach (var item in order.TMealOrderItems)
@@ -195,7 +196,6 @@ namespace gym_api.Controllers.meal
             if (order == null)
                 return BadRequest("沒有購物車");  
             order.FOrderStatus = "待付款";
-            order.FOrderAt = DateTime.Now;
             await _context.SaveChangesAsync();
 
             return Ok(order.FOrderId);
